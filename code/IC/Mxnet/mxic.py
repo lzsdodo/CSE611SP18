76c648ba57198c1adb7cd0389b63b7c630f57020
Smruti Venkatesh <36144476+smrutivenky@users.noreply.github.com>
2018-04-30 03:25:23 -0400

    Mxnet image classification
    Mnist

---
+++ code/IC/Mxnet/mxic.py
@@ -0,0 +1,136 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import mxnet as mx
+mnist = mx.test_utils.get_mnist()
+
+# Fix the seed
+mx.random.seed(42)
+
+# Set the compute context, GPU is available otherwise CPU
+ctx = mx.gpu() if mx.test_utils.list_gpus() else mx.cpu()
+
+
+# In[ ]:
+
+
+batch_size = 100
+train_iter = mx.io.NDArrayIter(mnist['train_data'], mnist['train_label'], batch_size, shuffle=True)
+val_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
+
+
+# In[ ]:
+
+
+data = mx.sym.var('data')
+# Flatten the data from 4-D shape into 2-D (batch_size, num_channel*width*height)
+data = mx.sym.flatten(data=data)
+
+
+# In[ ]:
+
+
+# The first fully-connected layer and the corresponding activation function
+fc1  = mx.sym.FullyConnected(data=data, num_hidden=128)
+act1 = mx.sym.Activation(data=fc1, act_type="relu")
+
+# The second fully-connected layer and the corresponding activation function
+fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 64)
+act2 = mx.sym.Activation(data=fc2, act_type="relu")
+
+
+# In[ ]:
+
+
+# MNIST has 10 classes
+fc3  = mx.sym.FullyConnected(data=act2, num_hidden=10)
+# Softmax with cross entropy loss
+mlp  = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
+
+
+# In[ ]:
+
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
+# create a trainable module on compute context
+mlp_model = mx.mod.Module(symbol=mlp, context=ctx)
+mlp_model.fit(train_iter,  # train data
+              eval_data=val_iter,  # validation data
+              optimizer='sgd',  # use SGD to train
+              optimizer_params={'learning_rate':0.1},  # use fixed learning rate
+              eval_metric='acc',  # report accuracy during training
+              batch_end_callback = mx.callback.Speedometer(batch_size, 100), # output progress for each 100 data batches
+              num_epoch=10)  # train for at most 10 dataset passes
+
+
+# In[ ]:
+
+
+test_iter = mx.io.NDArrayIter(mnist['test_data'], None, batch_size)
+prob = mlp_model.predict(test_iter)
+assert prob.shape == (10000, 10)
+
+
+# In[ ]:
+
+
+test_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
+# predict accuracy of mlp
+acc = mx.metric.Accuracy()
+mlp_model.score(test_iter, acc)
+print(acc)
+assert acc.get()[1] > 0.96, "Achieved accuracy (%f) is lower than expected (0.96)" % acc.get()[1]
+
+
+# In[ ]:
+
+
+data = mx.sym.var('data')
+# first conv layer
+conv1 = mx.sym.Convolution(data=data, kernel=(5,5), num_filter=20)
+tanh1 = mx.sym.Activation(data=conv1, act_type="tanh")
+pool1 = mx.sym.Pooling(data=tanh1, pool_type="max", kernel=(2,2), stride=(2,2))
+# second conv layer
+conv2 = mx.sym.Convolution(data=pool1, kernel=(5,5), num_filter=50)
+tanh2 = mx.sym.Activation(data=conv2, act_type="tanh")
+pool2 = mx.sym.Pooling(data=tanh2, pool_type="max", kernel=(2,2), stride=(2,2))
+# first fullc layer
+flatten = mx.sym.flatten(data=pool2)
+fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
+tanh3 = mx.sym.Activation(data=fc1, act_type="tanh")
+# second fullc
+fc2 = mx.sym.FullyConnected(data=tanh3, num_hidden=10)
+# softmax loss
+lenet = mx.sym.SoftmaxOutput(data=fc2, name='softmax')
+
+
+# In[ ]:
+
+
+lenet_model = mx.mod.Module(symbol=lenet, context=ctx)
+# train with the same
+lenet_model.fit(train_iter,
+                eval_data=val_iter,
+                optimizer='sgd',
+                optimizer_params={'learning_rate':0.1},
+                eval_metric='acc',
+                batch_end_callback = mx.callback.Speedometer(batch_size, 100),
+                num_epoch=10)
+
+
+# In[ ]:
+
+
+test_iter = mx.io.NDArrayIter(mnist['test_data'], None, batch_size)
+prob = lenet_model.predict(test_iter)
+test_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
+# predict accuracy for lenet
+acc = mx.metric.Accuracy()
+lenet_model.score(test_iter, acc)
+print(acc)
+assert acc.get()[1] > 0.98, "Achieved accuracy (%f) is lower than expected (0.98)" % acc.get()[1]
+
